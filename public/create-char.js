124e7895834e22e1c9bb92c0cd6f54b2a2bf486c
Colby Workman <cgw2319@gmail.com>
2021-11-23 12:29:35 -0700

    changed classes to get class options from api

--- public/create-char.js
+++ public/create-char.js
@@ -1,11 +1,14 @@
 // const { default: axios } = require("axios")
 
+// const { default: axios } = require("axios");
+
 
 let diceRoll = document.querySelector('#dice-roll');
 let raceBlocks = document.querySelector('.race-section');
 let testBtn = document.getElementById('test')
+let classSection = document.getElementById('class-blocks')
 
-let getRaces = (req, res) => {
+const getRaces = (req, res) => {
     axios.get('http://www.dnd5eapi.co/api/races')
     .then((res) => {
         const {results} = res.data
@@ -23,8 +26,27 @@ let getRaces = (req, res) => {
 
     }).catch((err) => console.log('somethins messed up'))
 }
-
 getRaces()
+
+const getClasses = (req, res) => {
+    axios.get('http://www.dnd5eapi.co/api/classes')
+    .then((res) => {
+        const {results} = res.data
+        console.log(results)
+        results.forEach((element, index) => {
+            let newClass = document.createElement('div')
+            newClass.classList.add('class-option')
+            newClass.innerHTML = `<h3>${results[index].name}</h3>`
+            newClass.id = results[index].index
+            newClass.addEventListener('click', () => {
+                console.log(`${newClass.id} clicked`)
+            })
+            classSection.appendChild(newClass)
+        })
+    })
+}
+getClasses()
+
 // diceRollScores()
 diceRoll.addEventListener('click', () => {
     axios.get('http://localhost:4004/attribute_scores')
