2f3846df3dc145605d54e634625b083f1e3eb744
Colby Workman <cgw2319@gmail.com>
2021-11-23 19:55:42 -0700

    added skills

--- public/create-char.js
+++ public/create-char.js
@@ -1,12 +1,13 @@
-// const { default: axios } = require("axios")
 
-// const { default: axios } = require("axios");
 
 
 let diceRoll = document.querySelector('#dice-roll');
 let raceBlocks = document.querySelector('.race-section');
 let testBtn = document.getElementById('test')
 let classSection = document.getElementById('class-blocks')
+let attributeSection = document.querySelector('.attributes-section')
+let attributeSectionTest = document.querySelector('.attributes-section-test')
+let skillsSection = document.querySelector('.skills-section')
 
 const getRaces = (req, res) => {
     axios.get('http://www.dnd5eapi.co/api/races')
@@ -47,10 +48,65 @@ const getClasses = (req, res) => {
 }
 getClasses()
 
+const getAttributes = (req, res) => {
+    axios.get('http://www.dnd5eapi.co/api/ability-scores')
+    .then((res) => {
+        const {results} = res.data
+        console.log(results)
+        results.forEach((attribute, index) => {
+            let abilityScore = document.createElement('div')
+            abilityScore.classList.add('ability-block')
+            abilityScore.innerHTML = 
+            `<h5>${results[index].name}</h5>
+             <p id='${results[index].index}'class='att-score'></p>
+             <h4></h4>`
+            abilityScore.addEventListener('click', () => {
+                console.log(`${abilityScore.id} clicked`)
+            })
+            attributeSection.appendChild(abilityScore)
+        })
+    })
+    .catch((err) => console.log(err))
+}
+getAttributes()
+
+const getSkills = (req, res) => {
+    axios.get('http://www.dnd5eapi.co/api/skills')
+    .then((res) => {
+        const {results} = res.data
+        console.log(results)
+        results.forEach((element, index) => {
+            let skillBlock = document.createElement('div')
+            skillBlock.classList.add('skill-block')
+            skillBlock.innerHTML = 
+            `<input type='checkbox' id='${results[index].index}' value='${results[index].name}'
+             <label for='${results[index].index}'>${results[index].name}</label><br>
+            `
+            let skillSelect = document.getElementById(results[index].index)
+            skillSelect.addEventListener('click', () => console.log('clkldsjf'))
+            skillsSection.appendChild(skillBlock)
+        })
+
+    })
+}
+getSkills()
+
 // diceRollScores()
 diceRoll.addEventListener('click', () => {
     axios.get('http://localhost:4004/attribute_scores')
     .then((res) => {
         console.log(res.data)
+        let charisma = document.getElementById('cha')
+        let constitution= document.getElementById('con')
+        let dexterity = document.getElementById('dex')
+        let intelligence = document.getElementById('int')
+        let strength = document.getElementById('str')
+        let wisdom = document.getElementById('wis')
+        charisma.textContent = res.data[0]
+        constitution.textContent = res.data[1]
+        dexterity.textContent = res.data[2]
+        intelligence.textContent = res.data[3]
+        strength.textContent = res.data[4]
+        wisdom.textContent = res.data[5]
     }).catch((err) => console.log(err))
 })
--- server/controller.js
+++ server/controller.js
@@ -17,18 +17,13 @@ module.exports = {
             rolls = rolls.sort()
             rolls.shift()
             let score = rolls[0] + rolls[1] + rolls[2]
-            scores.push(score)
+            if(score < 8){
+                score += 2
+                scores.push(score)
+            }else{
+                scores.push(score)
+            }
         }
         res.status(200).send(scores)
-    },
-    getRaces: async (req, res) => {
-        const {data: response} = await axios.get('http://www.dnd5eapi.co/api/races')
-       console.log(response.results)
-      return response.results
-        // .then((response) => {
-        //     let {data} = response
-        //     console.log(response.data);
-        //     response.status(200).send(response.data);
-        // }).catch((err) => console.log(err))
     }
 }
