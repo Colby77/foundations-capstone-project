dcfc8ec46a9d709797afb17b61cbad985361876b
Colby Workman <cgw2319@gmail.com>
2021-11-27 12:13:15 -0700

    db seed complete

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,21 @@
-const { default: axios } = require("axios");
 const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+
+const Sequelize = require("sequelize")
+const sqlize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
 
 module.exports = {
     diceRollScores: (req, res) => {
@@ -28,5 +40,43 @@ module.exports = {
             }
         }
         res.status(200).send(scores)
+    },
+    saveChar: (req, res) => {
+        console.log(req.body)
+        res.status(200).send('server side ok, sent to front')
+    },
+    seed: (req, res) => {
+        sqlize.query(`
+        drop table if exists characters;
+
+        create table characters(
+            character_id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            race VARCHAR(50),
+            class VARCHAR(50),
+            background VARCHAR(50),
+            alignment VARCHAR(50),
+            prof_bonus INTEGER,
+            cha INTEGER,
+            con INTEGER,
+            dex INTEGER,
+            intl INTEGER,
+            str INTEGER,
+            wis INTEGER,
+            profs VARCHAR(200),
+            langs VARCHAR(100),
+            spells VARCHAR(200)
+
+        );
+
+        INSERT INTO characters(name, race, class, background, alignment, prof_bonus,
+             cha, con, dex, intl, str, wis, profs, langs, spells)
+             VALUES('Imsh', 'Half-Orc', 'Bard', 'Soldier', 'Chaotic-Good',
+              2, 17, 11, 13, 8, 17, 12, 'Intimidation, Athletics, Persuasion, Performance', 'Orc, Common',
+              'Vicious Mockery, Prestidigitation, Minor Illusion');
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const {SERVER_PORT} = process.env
 
 const app = express()
 
-const { diceRollScores} = require('./controller.js')
+const {diceRollScores, saveChar, seed} = require('./controller.js')
 
 app.use(express.json())
 app.use(cors())
 
-const port = 4004
+app.post('/seed', seed)
 
 app.get('/attribute_scores', diceRollScores)
-app.post('/character_complete', (req, res) => {
-    console.log(req.body)
-    res.status(200).send('server side ok, sent to front')
-})
+app.post('/character_complete', saveChar)
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
